ece6a0e222fe65b96b2e375c968baaea2e7458a8
Nathan-Nay1 <nayone606@googlemail.com>
2022-06-27 19:56:31 +0100

    new additions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+import { useEffect } from 'react';
 
 function App() {
+
+  const [quotes, setQuotes] = useState("");
+
+  const getQuote = () => {
+    fetch("https://type.fit/api/quotes")
+    .then((response) => response.json())
+    .then((data) => {
+      let randomNum = Math.floor(Math.random() * data.length);
+      setQuotes(data[randomNum]);
+    });
+  };
+
+  useEffect(() => {
+    getQuote();
+  },[]);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="quote" id="quote-box">
+        <p id="text">"{quotes.text}"</p>
+        { (quotes.author === null) ?
+          <p id="author"> - anon</p> :
+          <p id="author">- {quotes.author}</p>}
+        <div className="btn-box">
+          <button onClick={getQuote} className="btn" id="new-quote">Get Quote</button>
+          <a 
+          href={`https://twitter.com/intent/tweet?text=${quotes.text} - ${quotes.author}`} 
+          target="_blank" 
+          rel="noopener noreferrer" 
+          className="btn" 
+          id="tweet-quote">Tweet</a>
+        </div>
+      </div>
     </div>
   );
 }
