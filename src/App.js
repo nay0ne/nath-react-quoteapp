c9641ebb605e276434eedcfa1f9559807067c4d1
Nathan-Nay1 <nayone606@googlemail.com>
2022-08-23 08:32:05 +0100

    change to api

--- src/App.js
+++ src/App.js
@@ -6,20 +6,18 @@ function App() {
 
   const [quotes, setQuotes] = useState("");
 
-  const getQuote = () => {
-    fetch("https://type.fit/api/quotes")
+    const getQuote = () => {
+    fetch("https://api.api-ninjas.com/v1/quotes?", {
+      method: 'GET',
+      headers: {
+        'X-API-KEY': 'r+tiyUGL3Lu6jxRJESW1Ow==Xctijubm6DnV6Wx5'
+      }
+    })
     .then((response) => response.json())
-    .then((data) => {
+    .then(data => {
       let randomNum = Math.floor(Math.random() * data.length);
       setQuotes(data[randomNum]);
     });
-  };
-
-  let quoteAuth;
-  if (quotes.author === null) {
-    quoteAuth = "Anon";
-  } else {
-    quoteAuth = quotes.author;
   };
 
   useEffect(() => {
@@ -29,12 +27,12 @@ function App() {
   return (
     <div className="App">
       <div className="quote" id="quote-box">
-        <p id="text">"{quotes.text}"</p>
-        <p id="author">- {quoteAuth}</p>
+        <p id="text">"{quotes.quote}"</p>
+        <p id="author">- {quotes.author}</p>
         <div className="btn-box">
           <button onClick={getQuote} className="btn" id="new-quote" aria-label="Get a new quote">Quote</button>
           <a 
-          href={`https://twitter.com/intent/tweet?text=${quotes.text} - ${quoteAuth}`} 
+          href={`https://twitter.com/intent/tweet?text=${quotes.quote} - ${quotes.author}`} 
           target="_blank" 
           rel="noopener noreferrer" 
           className="btn" 
